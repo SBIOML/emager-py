66da32a93bd94f853917db74addcaac726c80ec2
Michiboi29 <emichaud29@gmail.com>
2024-05-28 17:29:12 -0400

    colormatrix WIP

--- exemples/live_matrix.py
+++ exemples/live_matrix.py
@@ -1,1 +1,8 @@
-# TODO replace live_EMAGER.py
+from emager_py.streamers import SerialStreamer
+from emager_py.utils.find_usb import find_psoc
+from emager_py.visualisation.color_matrix import RealTimeMatrixPlot
+
+PORT = find_psoc()
+stream_client = SerialStreamer(PORT, 1500000)
+rt_plot = RealTimeMatrixPlot(stream_client, colormap='plasma', interval=300)
+rt_plot.show()
